a6e2b8433b7d0e399f23bdd09afb4df01c6d4ec7
Tomas Gonzalez <tfgonzalez@fi.uba.ar>
2024-06-26 15:36:36 -0300

    Add MenuItem

---
+++ src/MenuItem.ts
@@ -0,0 +1,11 @@
+class MenuItem {
+  quantity: number;
+  product: string;
+  price: number;
+
+  constructor(quantity: number, product: string, price: number) {
+    this.quantity = quantity;
+    this.product = product;
+    this.price = price;
+  }
+}
